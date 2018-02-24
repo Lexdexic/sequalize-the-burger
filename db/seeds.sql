c663d79e6264f71f09864f3d21325a200b3862d0
Jeremy <Lexdexic@gmail.com>
2018-02-23 20:36:03 -0700

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Big Mac');
+INSERT INTO burgers (burger_name) VALUES ('Truxican');
+INSERT INTO burgers (burger_name) VALUES ('Sirloin Burger');
+INSERT INTO burgers (burger_name) VALUES ('Bruce Lee');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheese Burger', true);
